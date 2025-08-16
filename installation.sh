418d856330766d16b80160b5b168d365952fd1bc
Arya-1-HR <aryagowda177@gmail.com>
2025-08-16 20:10:33 +0530

    Bash file correction

--- installation.sh
+++ installation.sh
@@ -7,7 +7,7 @@ INSTALLING_REPO="https://github.com/Arya-1-HR/Run-Dawg/raw/main/bin/dawg"
 echo "[*] Installing $APP_NAME from "$INSTALLING_REPO" to "$INSTALL_DIR" ..."
 curl -L "$INSTALLING_REPO" -o "$APP_NAME"
 sudo mv "$APP_NAME" "$INSTALL_DIR/"
-sudo chmod -x "$INSTALL_DIR/$APP_NAME"
+sudo chmod +x "$INSTALL_DIR/$APP_NAME"
 
 echo "[*] Installation complete! Running $APP_NAME..."
 echo "[*] Run command "$APP_NAME" to start"
