5a971c4a833e144598a2653538858a6cace4edbf
Arya-1-HR <aryagowda177@gmail.com>
2025-08-16 20:21:10 +0530

    Corrections in bash file

--- installation.sh
+++ installation.sh
@@ -8,6 +8,7 @@ echo "[*] Installing $APP_NAME from "$INSTALLING_REPO" to "$INSTALL_DIR" ..."
 curl -L "$INSTALLING_REPO" -o "$APP_NAME"
 sudo mv "$APP_NAME" "$INSTALL_DIR/"
 sudo chmod +x "$INSTALL_DIR/$APP_NAME"
+sudo xattr -d com.apple.quarantine "$INSTALL_DIR/$APP_NAME"
 
 echo "[*] Installation complete! Running $APP_NAME..."
 echo "[*] Run command "$APP_NAME" to start"
