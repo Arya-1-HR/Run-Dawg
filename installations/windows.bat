edd640b3b1687b27239f9d059ecdd0fdfbbe2a14
Arya-1-HR <aryagowda177@gmail.com>
2025-08-20 00:28:00 +0530

    Updated bat file

--- installations/windows.bat
+++ installations/windows.bat
@@ -1,25 +1,19 @@
 @echo off
 setlocal
 
-:: App name
 set APP_NAME=dawg.exe
 
-:: Install directory (you can change this if you want)
-set INSTALL_DIR=%ProgramFiles%\dawg
+set INSTALL_DIR=%LOCALAPPDATA%\dawg
 
-:: Download URL (raw GitHub link to your Windows build)
 set INSTALLING_REPO=https://github.com/Arya-1-HR/Run-Dawg/raw/main/bin/dawg.exe
 
 echo [*] Installing %APP_NAME% from %INSTALLING_REPO% ...
 
-:: Create install dir if not exists
 if not exist "%INSTALL_DIR%" mkdir "%INSTALL_DIR%"
 
-:: Download binary using curl (Windows 10+ has curl preinstalled)
 curl -L %INSTALLING_REPO% -o "%INSTALL_DIR%\%APP_NAME%"
 
-:: Add to PATH (system-wide)
-setx PATH "%PATH%;%INSTALL_DIR%" /M
+setx PATH "%INSTALL_DIR%;%PATH%"
 
 echo [*] Installation complete!
 echo [*] Open a new terminal and run "dawg" to start.
